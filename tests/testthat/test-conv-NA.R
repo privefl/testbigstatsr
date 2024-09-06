33ce2b621ee00ed24727c1bd0a95f553d6ef0f1a
Florian Privé <florian.prive.21@gmail.com>
2024-09-06 13:56:41 +0200

    also test with bigmemory

--- tests/testthat/test-conv-NA.R
+++ tests/testthat/test-conv-NA.R
@@ -3,7 +3,7 @@ test_that("dbl2int() works", {
   expect_identical(dbl2int(NA_real_), NA_integer_)
 })
 
-test_that("NA conversion to integer work", {
+test_that("NA conversion to integer work with FBMs", {
   X <- bigstatsr::FBM(1, 1, type = "integer", init = 7)
   expect_identical(X[1], 7L)
   X[1] <- NA
@@ -14,4 +14,15 @@ test_that("NA conversion to integer work", {
   expect_identical(X[1], NA_integer_)  # NOK
   X[1, 1] <- NA_real_
   expect_identical(X[1], NA_integer_)  # NOK
+})
+
+test_that("NA conversion to integer work with big.matrices", {
+  X <- bigmemory::big.matrix(1, 1, type = "integer", init = 7)
+  expect_identical(X[1], 7L)
+  X[1, 1] <- NA
+  expect_identical(X[1, 1], NA_integer_)  # OK
+  X[1, 1] <- NA_integer_
+  expect_identical(X[1, 1], NA_integer_)  # OK
+  expect_warning(X[1, 1] <- NA_real_)
+  expect_identical(X[1, 1], NA_integer_)  # NOK
 })
