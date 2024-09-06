a5f850c92883842cfc9a1efba14ef8a01ffbae72
Florian Privé <florian.prive.21@gmail.com>
2024-09-06 09:39:01 +0200

    init to 7

--- tests/testthat/test-conv-NA.R
+++ tests/testthat/test-conv-NA.R
@@ -1,5 +1,5 @@
 test_that("Downcast warnings work", {
-  X <- bigstatsr::FBM(1, 1, type = "integer")
+  X <- bigstatsr::FBM(1, 1, type = "integer", init = 7)
   X[1, 1] <- NA_real_
   expect_identical(X[1], NA_integer_)
   X[1] <- NA_real_
