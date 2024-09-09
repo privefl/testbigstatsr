d60325ee10f7ebadba823c629c6fd2dd3801e9eb
Florian Privé <florian.prive.21@gmail.com>
2024-09-09 15:55:18 +0200

    where is the error then?

--- tests/testthat/test-bigmemory.R
+++ tests/testthat/test-bigmemory.R
@@ -1,11 +1,17 @@
 test_that("Conversion to big.matrix works", {
 
-  X <- bigstatsr::FBM(10, 10, type = "double", init = 0)
-  X[1] <- NA
+  for (t in c("raw", "integer", "float", "double")) {
 
-  # Conversion works
-  expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
-  expect_s4_class(X2 <- X$bm(), "big.matrix")
-  expect_identical(typeof(X2), typeof(X))
-  expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
+    print(t)
+
+    X <- bigstatsr::FBM(10, 10, type = t, init = 0)
+    if (t != "raw") X[1] <- NA
+
+    # Conversion works
+    expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
+    expect_s4_class(X2 <- X$bm(), "big.matrix")
+    expect_identical(typeof(X2), typeof(X))
+    if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
+  }
+
 })
