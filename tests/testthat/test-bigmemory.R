264558fe6f1f647ecdedfa7eee2e8153d1346411
Florian Privé <florian.prive.21@gmail.com>
2024-09-09 16:27:31 +0200

    more printing

--- tests/testthat/test-bigmemory.R
+++ tests/testthat/test-bigmemory.R
@@ -12,6 +12,9 @@ test_that("Conversion to big.matrix works", {
   expect_s4_class(X2 <- X$bm(), "big.matrix")
   print(X2[])
   expect_identical(typeof(X2), typeof(X))
+  print(X2[1])
+  print(X2[1, 1])
   expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
+  expect_equal(as.numeric(X2[1, 1]), as.numeric(X[1, 1]))
 
 })
