856cf16cd06610c6de7e94618dd96c051c2ad344
Florian Privé <florian.prive.21@gmail.com>
2024-09-09 16:09:02 +0200

    where is the issue?

--- tests/testthat/test-bigmemory.R
+++ tests/testthat/test-bigmemory.R
@@ -1,17 +1,46 @@
 test_that("Conversion to big.matrix works", {
 
-  for (t in c("raw", "integer", "float", "double")) {
+  t <- "raw"
+  X <- bigstatsr::FBM(10, 10, type = t, init = 0)
+  if (t != "raw") X[1] <- NA
 
-    print(t)
+  # Conversion works
+  expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
+  expect_s4_class(X2 <- X$bm(), "big.matrix")
+  expect_identical(typeof(X2), typeof(X))
+  if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
 
-    X <- bigstatsr::FBM(10, 10, type = t, init = 0)
-    if (t != "raw") X[1] <- NA
 
-    # Conversion works
-    expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
-    expect_s4_class(X2 <- X$bm(), "big.matrix")
-    expect_identical(typeof(X2), typeof(X))
-    if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
-  }
+  t <- "integer"
+  X <- bigstatsr::FBM(10, 10, type = t, init = 0)
+  if (t != "raw") X[1] <- NA
+
+  # Conversion works
+  expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
+  expect_s4_class(X2 <- X$bm(), "big.matrix")
+  expect_identical(typeof(X2), typeof(X))
+  if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
+
+
+  t <- "float"
+  X <- bigstatsr::FBM(10, 10, type = t, init = 0)
+  if (t != "raw") X[1] <- NA
+
+  # Conversion works
+  expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
+  expect_s4_class(X2 <- X$bm(), "big.matrix")
+  expect_identical(typeof(X2), typeof(X))
+  if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
+
+
+  t <- "double"
+  X <- bigstatsr::FBM(10, 10, type = t, init = 0)
+  if (t != "raw") X[1] <- NA
+
+  # Conversion works
+  expect_s4_class(X$bm.desc(), "big.matrix.descriptor")
+  expect_s4_class(X2 <- X$bm(), "big.matrix")
+  expect_identical(typeof(X2), typeof(X))
+  if (t != "float") expect_equal(as.numeric(X2[1]), as.numeric(X[1]))
 
 })
